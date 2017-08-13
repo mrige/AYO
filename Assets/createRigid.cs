13bfa6750de7be592d4471163d45076257c8a425
mrige <thorbaba01@gmail.com>
2017-08-12 22:47:18 -0500

    First Commit

---
+++ Assets/createRigid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createRigid : MonoBehaviour {
+
+    public Transform spawnPoint;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.AddComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < spawnPoint.position.y - 10)
+        {
+            transform.position = spawnPoint.position;
+            rb.velocity = Vector3.zero;
+        }
+	}
+}
